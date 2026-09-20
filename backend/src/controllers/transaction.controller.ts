9b1daef6a4044c116505c16d4720d42eab4b991b
Jai Athiyaa <jaiathiyaa183@gmail.com>
2026-09-20 15:51:05 +0530

    Validated Transaction GET and POST

--- backend/src/controllers/transaction.controller.ts
+++ backend/src/controllers/transaction.controller.ts
@@ -37,17 +37,26 @@ export const getTransactionById = async (req: Request, res: Response) => {
 export const createTransaction = async (req: Request, res: Response) =>{
     try{
         const { title , description , amount, category , type , date } = req.body;
-        if (!title || !description || !amount || !category || !type || !date) {
-            return res.status(400).json({ error: "All fields are required." });
+        if (!title || amount === undefined || amount === null || !type) {
+            return res.status(400).json({
+                error: "Title, amount and type are required."
+            });
         }
         if( type !== "EXPENSE" && type !== "INCOME"){
             return res.status(400).json({ error: "Type must be either 'EXPENSE' or 'INCOME'." });
+        }
+        const numericAmount = Number(amount);
+
+        if (isNaN(numericAmount) || numericAmount <= 0) {
+            return res.status(400).json({
+                error: "Amount must be a valid positive number."
+            });
         }
         const newTransaction = await prisma.transaction.create({
             data :{
                 title,
                 description,
-                amount: Number(amount),
+                amount: numericAmount,
                 category,
                 type,
                 date: date ? new Date(date) : new Date(),
